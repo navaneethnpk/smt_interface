18723fecea28590dc40889066894f279bdba9f2d
navaneethnpk <navaneethpkrasikapriya.npk@gmail.com>
2023-11-23 16:24:29 +0530

    interface columns changed

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
-rows = [{'id':1, 'ast':'430225512.0', 'utc':'2023-08-20 11:05:12', 'detector':'CZT', 'binning':'0.1', 'obsid':'9000005822', 'orbit':'42682', 'plot':'static/czti_430225512.0_0.1.png', 'type':'GRB'}, {'id':2, 'ast':'430225512.0', 'utc':'2023-08-20 11:05:12', 'detector':'VETO', 'binning':'1.0', 'obsid':'9000005822', 'orbit':'42682', 'plot':'static/veto_430225512.0_1.0.png', 'type':'GRB'}, {'id':3, 'ast':'429715396.0', 'utc':'2023-08-14 13:23:16', 'detector':'VETO', 'binning':'1.0', 'obsid':'9000005818', 'orbit':'42682', 'plot':'static/veto_429715396.0_1.0.png', 'type':'TGF'}]
+# rows = [{'no':1, 'ast':'434248071.0','utc':'2023-10-06 00:27:50','obsid':'9000005870', 'orbit':'43373', 'type':''},
+#         {'no':2, 'ast':'438328242.0','utc':'2023-11-22 05:50:41','obsid':'9000005940', 'orbit':'44073', 'type':''},
+#         {'no':3, 'ast':'438361962.0','utc':'2023-11-22 15:12:41','obsid':'9000005940', 'orbit':'44078', 'type':''}]
+
+#Connect to the SQLite database
+def getdatadb():
+    conn = sqlite3.connect('smttriggers.db')
+    cursor = conn.cursor()
+    query = "SELECT * FROM triggers;"
+    cursor.execute(query)
+    dbrow = cursor.fetchall()
+    columns = [desc[0] for desc in cursor.description]
+    rowdata = []
+    for row in dbrow:
+        row_dict = dict(zip(columns, row))
+        rowdata.append(row_dict)
+        # print(row_dict)
+    # print(rowdata)
+    cursor.close()
+    conn.close()
+    return rowdata
 
 @app.route("/")
 def hello_czti():
+    rows = getdatadb()
     return render_template('home.html', elem=rows)
 
 if __name__ == '__main__':
--- sqldb.py
+++ sqldb.py
@@ -1,10 +1,27 @@
 # Auther: NavaneethPK, V1.0
 import sqlite3
 
-#Creating the table & database
+def sqladdrow(tlist):
+	status = False
+	try:
+		conn = sqlite3.connect('smttriggers.db')
+		cursor = conn.cursor()
+		trigger_info = tlist
+		query = "INSERT INTO triggers (ast, utc, obsid, orbit) VALUES (?, ?, ?, ?);"
+		cursor.execute(query, trigger_info)
+		conn.commit()
+		status = True
+	except sqlite3.Error as e:
+		print("Error adding trigger data to the database: {}".format(e))
+	finally:
+		cursor.close()
+		conn.close()
+	return status
+
+# # Creating the table & database
 # conn = sqlite3.connect('smttriggers.db')
 # cursor = conn.cursor()
-# cursor.execute("""CREATE TABLE triggers (no INTEGER PRIMARY KEY AUTOINCREMENT, ast TEXT, utc TEXT, detector TEXT, binning TEXT, obsid TEXT, orbit TEXT, plot TEXT, type TEXT)""")
+# cursor.execute("""CREATE TABLE triggers (no INTEGER PRIMARY KEY AUTOINCREMENT, ast TEXT, utc TEXT, obsid TEXT, orbit TEXT, type TEXT)""")
 
 #Reading the table
 conn = sqlite3.connect('smttriggers.db')
@@ -16,23 +33,6 @@ for row in rows:
 	print(row)
 cursor.close()
 conn.close()
-
-def sqladdrow(tlist):
-	status = False
-	try:
-		conn = sqlite3.connect('smttriggers.db')
-		cursor = conn.cursor()
-		trigger_info = tlist
-		query = "INSERT INTO triggers (ast, utc, detector, binning, obsid, orbit, plot, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
-		cursor.execute(query, trigger_info)
-		conn.commit()
-		status = True
-	except sqlite3.Error as e:
-		print("Error adding trigger data to the database: {}".format(e))
-	finally:
-		cursor.close()
-		conn.close()
-	return status
 
 #Removing a row from the database table
 # row_id = 3
